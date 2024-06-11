19565bf07c37d2b03cb3a6681f6226cd464c438c
Ta Huynh Dat <tadat216@gmail.com>
2024-06-11 15:22:48 +0700

    update

---
+++ get_player_trophies_record_data.py
@@ -0,0 +1,16 @@
+import requests
+from datetime import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import storage
+import os
+
+# for key, value in os.environ.items():
+#     print(f'{key}: {value}')
+
+try:
+    json_private_key = os.environ["JSON_PRIVATE_KEY"]
+except KeyError:
+    json_private_key = None
+print(json_private_key)
